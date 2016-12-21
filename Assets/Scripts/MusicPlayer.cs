3ee5f5eccf4042f87cb9ee1c1ad70dfc5c19e7c9
MOHAMMAD TOFI <mohammad.tofi@gmail.com>
2016-12-21 22:56:05 +0300

    Fu: add sound and physical move to the ball in the game

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null; 
+	// Use this for initialization
+	void  Awake() 
+	{
+		print ("the awake method is call");
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("the game object is distroy");
+		}
+		{
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);	
+		}
+	}
+	void Start () {
+		Debug.Log("music player start "+ GetInstanceID());
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
